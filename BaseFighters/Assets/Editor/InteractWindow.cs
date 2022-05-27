86200a3cb3b8610ac5c40b7b6bf70d1d687ed7c6
Laharnar <marko.laharnar@gmail.com>
2022-05-27 23:08:04 +0200

    Update scene to have fighters, add search.

--- BaseFighters/Assets/Editor/InteractWindow.cs
+++ BaseFighters/Assets/Editor/InteractWindow.cs
@@ -1,3 +1,8 @@
+using System.Net.Http.Headers;
+using System.IO;
+using System.Runtime.CompilerServices;
+using System.Data;
+using System;
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
@@ -10,6 +15,10 @@ public class InteractWindow:EditorWindow{
     Vector2 scroll;
     int time;
     bool[] foldouts = new bool[300];
+    GameObject manualRoot;
+    string searchObj="";
+    string searchRule="";
+    string searchLine="";
 
     // Add menu named "My Window" to the Window menu
     [MenuItem("DEV/Interact")]
@@ -20,42 +29,76 @@ public class InteractWindow:EditorWindow{
         window.Show();
     }
 
+    bool ContainsSearch(List<string> list, string search){
+        if(searchLine!= ""){
+            for (int i = 0; i < list.Count; i++){
+                if(list[i].ToLower().Contains(searchLine)) 
+                    return true;
+            } 
+        }
+        return false;
+    }
+
     void DrawItems(List<string> list, string label = ""){
-        if(label != "" && list.Count > 0)
+        if(label != "" && list.Count > 0 )
             EditorGUILayout.LabelField(label);
         EditorGUI.indentLevel ++;
-        for (int i = 0; i < list.Count; i++)
+    
+        for (int i = 0; i < list.Count; i++){
             list[i] = EditorGUILayout.TextField(list[i]);
+        }
         EditorGUI.indentLevel --;
+    }
+
+    static void ClearField(string label, ref string value){
+        EditorGUILayout.BeginHorizontal();
+        if(GUILayout.Button("x", GUILayout.Width(26))){
+            value = "";
+        }
+        value = EditorGUILayout.TextField(label, value).ToLower();
+        EditorGUILayout.EndHorizontal();
     }
 
     void OnGUI()
     {
-        if(GUILayout.Button("refresh |"+time) || time + 5 < Time.time){
+        manualRoot = (GameObject)EditorGUILayout.ObjectField(manualRoot, typeof(GameObject), true);
+        if (GUILayout.Button("refresh |"+time) || time + 5 < Time.time){
             time = (int)Time.time;
             objs.Clear();
-            objs.AddRange(GameObject.FindObjectsOfType<InteractState>());
+            if(manualRoot == null)
+                objs.AddRange(FindObjectsOfType<InteractState>());
+            else objs.AddRange(manualRoot.GetComponentsInChildren<InteractState>());
         }
+        ClearField("object  *..", ref searchObj);
+        ClearField("rule  *..", ref searchRule);
+        ClearField("line  *", ref searchLine);
+
         scroll = EditorGUILayout.BeginScrollView(scroll);
 
         GUILayout.Label("roots");
         int count = 0;
         for(int i = 0; i < objs.Count; i++)
         {
-            if(objs[i] == null)continue;
-            if(objs[i].transform.parent == null){
+            var obj = objs[i];
+            if(obj == null) continue;
+            var tobj = obj.transform;
+            if(!PrefixSearch(tobj.name, searchObj)) continue;
+
+            if(tobj.parent == null || (manualRoot!= null && tobj.parent == manualRoot.transform)){
                 EditorGUILayout.BeginHorizontal();
-                EditorGUILayout.ObjectField(objs[i], typeof(InteractState), true);
+                EditorGUILayout.ObjectField(obj, typeof(InteractState), true);
                 foldouts[i] = EditorGUILayout.Toggle(foldouts[i]);
                 EditorGUILayout.EndHorizontal();
 
                 if(!foldouts[i])
                     continue;
-                objs[i].ValidateComponents();
+                obj.ValidateComponents();
                 EditorGUI.indentLevel ++;
-                for (int j = 0; j < objs[i].module.triggerRules.Count; j++)
+                for (int j = 0; j < obj.module.triggerRules.Count; j++)
                 {
-                    var rule = objs[i].module.triggerRules[j];
+                    var rule = obj.module.triggerRules[j];
+                    if(!PrefixSearch(rule.rules.name, searchRule)) continue;
+
                     EditorGUILayout.ObjectField(rule.rules, typeof(InteractRuleset), false);
                     EditorGUILayout.LabelField(rule.trigger);
                     EditorGUI.indentLevel ++;
@@ -63,9 +106,15 @@ public class InteractWindow:EditorWindow{
                     {
                         if(action.note!= "")
                             EditorGUILayout.LabelField($"<{action.note}");
-                        DrawItems(action.action.conditions, "if");
-                        DrawItems(action.action.codes, "do");
-                        DrawItems(action.action.elseCodes, "elsedo");
+                        var draw = searchLine == "" 
+                        || ContainsSearch(action.action.conditions, searchLine)
+                        || ContainsSearch(action.action.codes, searchLine)
+                        || ContainsSearch(action.action.elseCodes, searchLine);
+                        if(draw){
+                            DrawItems(action.action.conditions, "if");
+                            DrawItems(action.action.codes, "do");
+                            DrawItems(action.action.elseCodes, "elsedo");
+                        }
                     }
                     EditorGUI.indentLevel --;
                 }
@@ -81,8 +130,10 @@ public class InteractWindow:EditorWindow{
         GUILayout.Label("children");
         for(int i = 0; i < objs.Count; i++)
         {
-            if(objs[i] == null)continue;
-            if(objs[i].transform.parent != null){
+            if(objs[i] == null) continue;
+            var tobj = objs[i].transform;
+            if(!PrefixSearch(tobj.name, searchObj)) continue;
+            if(tobj.parent != null && (manualRoot == null || tobj.parent != manualRoot.transform)){
                 count++;
                 EditorGUILayout.ObjectField(objs[i], typeof(InteractState), true);
             }
@@ -91,6 +142,10 @@ public class InteractWindow:EditorWindow{
         }
 
         EditorGUILayout.EndScrollView();
+    }
+
+    bool PrefixSearch(string longName, string search){
+        return search == "" || longName.ToLower().StartsWith(search);
     }
 
     private void OnEnable()
